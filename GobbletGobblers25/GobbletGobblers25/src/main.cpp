59fb135d07a7afb62d874171371f4eed2be6ffb1
HaimGoldman <haim.goldman@mail.huji.ac.il>
2025-01-02 18:11:47 +0200

    Add sample image and SDL code for display testing

--- GobbletGobblers25/GobbletGobblers25/src/main.cpp
+++ GobbletGobblers25/GobbletGobblers25/src/main.cpp
@@ -1,61 +1,108 @@
-
-/*This source code copyrighted by Lazy Foo' Productions 2004-2024
-and may not be redistributed without written permission.*/
-
-//Using SDL and standard IO
 #include <SDL.h>
-#include <stdio.h>
+#include <SDL_image.h>
+#include <iostream>
+#include <windows.h> //GetCurrentDirectory
 
-//Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main(int argc, char* args[])
-{
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
-
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
-
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
-		{
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
-
-			//Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0xFF));
-
-			//Update the surface
-			SDL_UpdateWindowSurface(window);
-
-			//Hack to get window to stay up
-			SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
-		}
-	}
-
-	//Destroy window
-	SDL_DestroyWindow(window);
-
-	//Quit SDL subsystems
-	SDL_Quit();
-
-	return 0;
+void waitForUserInput() {
+    std::cout << "Press Enter to continue..." << std::endl;
+    std::cin.ignore();
 }
 
 
+void printCurrentWorkingDirectory() {
+    char cwd[1024];
+    if (GetCurrentDirectoryA(sizeof(cwd), cwd)) {
+        std::cout << "Current working dir: " << cwd << std::endl;
+    }
+    else {
+        std::cerr << "Error getting current directory" << std::endl;
+    }
+}
 
+int main(int argc, char* args[])
+{
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+    SDL_Texture* texture = NULL;
 
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        waitForUserInput();
+        return 1;
+    }
+
+    window = SDL_CreateWindow("SDL Image Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL)
+    {
+        std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        waitForUserInput();
+        return 1;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL)
+    {
+        std::cout << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        waitForUserInput();
+        return 1;
+    }
+
+    int imgFlags = IMG_INIT_PNG;
+    if (!(IMG_Init(imgFlags) & imgFlags))
+    {
+        std::cout << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError() << std::endl;
+        waitForUserInput();
+        return 1;
+    }
+
+    printCurrentWorkingDirectory();
+
+    SDL_Surface* loadedSurface = IMG_Load("Resources/PNG_transparency_demonstration_1.png");
+    if (loadedSurface == NULL)
+    {
+        std::cout << "Unable to load image! SDL_image Error: " << IMG_GetError() << std::endl;
+        waitForUserInput();
+        return 1;
+    }
+
+    texture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+    if (texture == NULL)
+    {
+        std::cout << "Unable to create texture from image! SDL Error: " << SDL_GetError() << std::endl;
+        waitForUserInput();
+        return 1;
+    }
+
+    SDL_FreeSurface(loadedSurface);
+
+    SDL_Event e;
+    bool quit = false;
+
+    while (!quit)
+    {
+        while (SDL_PollEvent(&e) != 0)
+        {
+            if (e.type == SDL_QUIT)
+            {
+                quit = true;
+            }
+        }
+
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    IMG_Quit();
+    SDL_Quit();
+
+    return 0;
+}
