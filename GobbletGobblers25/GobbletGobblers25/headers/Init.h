1ec11230e8e8ce00d3572d7dec09b86582d9d570
HaimGoldman <haim.goldman@mail.huji.ac.il>
2025-01-05 13:54:59 +0200

    delete num of rows and columns from gameConstatns to make them variables

--- GobbletGobblers25/GobbletGobblers25/headers/Init.h
+++ GobbletGobblers25/GobbletGobblers25/headers/Init.h
@@ -4,11 +4,15 @@ namespace GameConstants
 {
 	const size_t MIN_ROWS = 3;
 	const size_t MIN_COLS = 3;
-	const int BOARD_ROWS = 3;
-	const int BOARD_COLS = 3;
 
+	const size_t MAX_ROWS = 3;
+	const size_t MAX_COLS = 3;
+
+	// This 2 constants are true while BOT considared as a player
+	const size_t MIN_PLAYERS = 2;
 	const size_t MAX_PLAYERS = 5;
-	const int PLAYERS = 2;
+
+	const size_t PLAYERS = 2;
 
 	const int GOBBLERS_PER_PLAYER = 6;
 	const int LARGE_GOBBLERS = 2;
