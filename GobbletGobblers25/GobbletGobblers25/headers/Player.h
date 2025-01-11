b04d72d742e000eac3c139644624f89e09665fdc
HaimGoldman <haim.goldman@mail.huji.ac.il>
2025-01-11 18:23:49 +0200

    Made Player class a template class

--- GobbletGobblers25/GobbletGobblers25/headers/Player.h
+++ GobbletGobblers25/GobbletGobblers25/headers/Player.h
@@ -1,29 +1,14 @@
 #pragma once
 
-#include <vector>
 #include "Gobbler.h"
 #include "Enums.h"
 
+
+template<typename T>
 class Player
 {
 public:
-    /**
-     * @brief Constructs a Player with a specified type and color.
-     * @param type The type of the Player (PlayerType type).
-     * @param color The color of the Player (PlayerColor type).
-     */
-    explicit Player(PlayerType type, PlayerColor color);
-
-    /**
-     * @brief Deleted copy constructor.
-     * The Player object cannot be copied, ensuring no unexpected copies are made.
-     */
-    Player(const Player&) = default; // std::vector.push_back
-
-    /**
-     * @brief Default destructor.
-     * Cleans up resources when the Player object is destroyed.
-     */
+    explicit Player(PlayerType playerType, T playerIdentfier);
     ~Player() = default;
 
     /**
@@ -33,75 +18,26 @@ public:
     PlayerType getType() const;
 
     /**
-     * @brief Gets the color of the Player.
-     * @return The color of the Player (PlayerColor type).
-     */
-    PlayerColor getColor() const;
-
-    /**
-     * @brief Gets a pointer to a Gobbler in the Player's hand at the specified index.
-     * @param index The index of the Gobbler in the Player's hand.
-     * @return A pointer to the Gobbler at the given index in the hand.
-     * @throws std::out_of_range If the index is out of bounds for the hand vector.
-     */
-    Gobbler* getGobblerFromHand(size_t index) const;
-
-    /**
      * @brief Sets the type of the Player.
      * @param type The type to set for the Player (PlayerType type).
      */
     void setType(PlayerType type);
 
     /**
-     * @brief Sets the color of the Player.
-     * @param color The color to set for the Player (PlayerColor type).
+     * @brief Gets the identifier of the Player.
+     * @return The identifier of the Player (T type).
      */
-    void setColor(PlayerColor color);
+    T getIdentifier() const;
 
     /**
-     * @brief Removes a Gobbler from the Player's hand at the specified index.
-     * @param index The index of the Gobbler to remove.
-     * @throws std::out_of_range If the index is out of bounds for the hand vector.
+     * @brief Sets the identifier of the Player.
+     * @param identifier The identifier to set for the Player (T type).
      */
-    void removeGobblerFromHand(size_t index);
-
-    /**
-     * @brief Inserts all Gobblers into the Player's hand.
-     * This method ensures that all Gobblers are placed into the hand vector.
-     */
-    void insertAllGobblersToHand();
+    void setIdentifier(T playerIdentifier);
 
 private:
-    /**
-     * @brief Initializes the hand of the Player.
-     * Prepares the Player's hand with a certain number of Gobblers.
-     */
-    void initHand();
-
-    /**
-     * @brief Creates Gobblers for the Player.
-     * Initializes the Gobblers that will be available for the Player's hand.
-     */
-    void createGobblers();
-
-    /**
-     * @brief Creates a specific number of Gobblers of a given size.
-     * @param size: The size of the Gobblers to create (GobblerSize type).
-     * @param numOfGobblers: The number of Gobblers to create of the specified size.
-     *
-     * This function creates and adds a specified number of Gobblers of a given size
-     * to the Player's collection of Gobblers (m_gobblers).
-     */
-    void createGobblers(GobblerSize size, size_t numOfGobblers);
-
-private:
-    std::vector<Gobbler> m_gobblers; /**< The collection of all Gobblers available to the Player. */
-    std::vector<Gobbler*> m_hand;    /**< The Player's hand containing pointers to Gobblers. */
-    PlayerType m_type;               /**< The type of the Player (PlayerType type). */
-    PlayerColor m_color;             /**< The color of the Player (PlayerColor type). */
+    PlayerType m_playerType; /**< The type of the Player (PlayerType type). */
+    T m_playerIdentifier;    /**< The identifier of the Player (T type). */
 };
 
-
-
-
-
+#include "Player.inl"
